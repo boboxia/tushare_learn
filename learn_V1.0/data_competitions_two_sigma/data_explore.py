de4b27992bd97d982506e50821e5222f79f7d92b
zhoulinbin <872551601@qq.com>
2018-12-07 00:13:14 +0800

    完成 group  by uid  和uid +xx的特征提取，包括，金额和余额

--- learn_V1.0/data_competitions_two_sigma/data_explore.py
+++ learn_V1.0/data_competitions_two_sigma/data_explore.py
@@ -66,19 +66,26 @@ if __name__=='__main__':
     #predict_pseudo_test()
     train_X_tr = pd.read_csv('D:/transaction_risk_competition/transaction_train_new.csv')
     #去掉'geo_code'列因为不好分
+    # 看空值分布
+    len_count = len(train_X_tr['UID'])
+    for col in ['UID', 'day', 'time', 'trans_amt', 'bal', 'acc_id1', 'acc_id2', 'acc_id3', 'amt_src1', 'amt_src2',
+                'trans_type2', 'trans_type1', 'market_code', 'market_type', 'merchant', 'code1', 'code2', 'channel',
+                'device_code1', 'device_code2', 'device_code3', 'device1', 'device2', 'mac1', 'ip1', 'ip1_sub']:
+        print('{0}'.format(col), '%.2f' % (train_X_tr[col].isnull().sum() / len_count))
+
+    train_X_tr['merchant'].isnull().sum() / len(train_X_tr['UID'])  # code1,code2,device_code3缺少太多，删掉这些列
+
     train_X_tr = train_X_tr[
         ['UID', 'day', 'time', 'trans_amt', 'bal', 'acc_id1', 'acc_id2', 'acc_id3', 'amt_src1', 'amt_src2',
-         'trans_type2', 'trans_type1', 'market_code', 'market_type', 'merchant', 'code1', 'code2', 'channel',
-         'device_code1', 'device_code2', 'device_code3', 'device1', 'device2', 'mac1', 'ip1', 'ip1_sub']]
+         'trans_type2', 'trans_type1', 'market_code', 'market_type', 'merchant', 'channel',
+         'device_code1', 'device_code2', 'device1', 'device2', 'mac1', 'ip1', 'ip1_sub']]
     print(train_X_tr.head(10))
     # 显示所有列显示所有行
     pd.set_option('display.max_columns', None)
     # pd.set_option('display.max_rows', None)
     pd.set_option('max_colwidth', 100)
-    # 看空值分布
-    len_count=len(train_X_tr['UID'])
-    for col in ['UID','day','time','trans_amt','bal','acc_id1','acc_id2','acc_id3','amt_src1','amt_src2','trans_type2','trans_type1','market_code','market_type','merchant','code1','code2','channel','device_code1','device_code2','device_code3','device1','device2','mac1','ip1','ip1_sub']:
-         print('{0}'.format(col),  '%.2f' %(train_X_tr[col].isnull().sum()/len_count))
+
+
 
     # 选取做因子化factorize，把几个可枚举的字符串转为了数字，并返回一个（字符串，数字）的对应映射表方便检查
     ##amt_src1 交易资金来源类型，余额，花呗做因子化
@@ -169,6 +176,107 @@ if __name__=='__main__':
     train_X_tr['amt_src1'] = train_X_tr['amt_src1'].replace(a, b)
     train_X_tr['amt_src1'].head(10)
     #至此完成 amt_src1的交易资金来源类型，余额，花呗做因子化，后面做groupby[uid,amt_src1]
+
+
+    ##开始构造特征：先不区分日期，也就是全日期
+    #1，groupby uid 取计数count,次数
+    train_X_tr_uid1_count = train_X_tr[
+        ['acc_id1', 'acc_id2', 'acc_id3', 'amt_src1', 'amt_src2', 'trans_type2', 'trans_type1', 'market_code',
+         'market_type', 'merchant', 'channel', 'device_code1', 'device_code2', 'device1', 'device2', 'mac1', 'ip1',
+         'ip1_sub']].groupby(train_X_tr['UID']).count().add_suffix('_count').reset_index()
+    # train_X_tr_uid1_count.head(5)
+
+    #2，groupby uid，xx(因子量)取金额均值（最大，最小等等）
+    train_X_tr_uid2_channel_bal = train_X_tr.groupby(['UID', 'channel'])['trans_bal'].agg(
+        {'mean', 'max', 'min'}).add_prefix(
+        'bal_channel_').unstack()
+    train_X_tr_uid2_channel_bal.columns = [x[0] + "_" + str(x[1]) for x in train_X_tr_uid2_channel_bal.columns.ravel()]
+    train_X_tr_uid2_channel_bal.reset_index(inplace=True)
+    train_X_tr_uid2_channel_bal.fillna(0.0, inplace=True)
+    print(train_X_tr_uid2_channel_bal.head(5))
+
+    train_X_tr_uid2_market_type_bal = train_X_tr.groupby(['UID', 'market_type'])['trans_bal'].agg(
+        {'mean', 'max', 'min'}).add_prefix(
+        'bal_market_type_').unstack()
+    train_X_tr_uid2_market_type_bal.columns = [x[0] + "_" + str(x[1]) for x in
+                                               train_X_tr_uid2_market_type_bal.columns.ravel()]
+    train_X_tr_uid2_market_type_bal.reset_index(inplace=True)
+    train_X_tr_uid2_market_type_bal.fillna(0.0, inplace=True)
+    print(train_X_tr_uid2_market_type_bal.head(5))
+
+    train_X_tr_uid2_trans_type2_bal = train_X_tr.groupby(['UID', 'trans_type2'])['trans_bal'].agg(
+        {'mean', 'max', 'min'}).add_prefix(
+        'bal_trans_type2_').unstack()
+    train_X_tr_uid2_trans_type2_bal.columns = [x[0] + "_" + str(x[1]) for x in
+                                               train_X_tr_uid2_trans_type2_bal.columns.ravel()]
+    train_X_tr_uid2_trans_type2_bal.reset_index(inplace=True)
+    train_X_tr_uid2_trans_type2_bal.fillna(0.0, inplace=True)
+    print(train_X_tr_uid2_trans_type2_bal.head(5))
+
+    train_X_tr_uid2_trans_type1_bal = train_X_tr.groupby(['UID', 'trans_type1'])['trans_bal'].agg(
+        {'mean', 'max', 'min'}).add_prefix(
+        'bal_trans_type1_').unstack()
+    train_X_tr_uid2_trans_type1_bal.columns = [x[0] + "_" + str(x[1]) for x in
+                                               train_X_tr_uid2_trans_type1_bal.columns.ravel()]
+    train_X_tr_uid2_trans_type1_bal.reset_index(inplace=True)
+    train_X_tr_uid2_trans_type1_bal.fillna(0.0, inplace=True)
+    print(train_X_tr_uid2_trans_type1_bal.head(5))
+
+    train_X_tr_uid2_bal_src1_bal = train_X_tr.groupby(['UID', 'bal_src1'])['trans_bal'].agg(
+        {'mean', 'max', 'min'}).add_prefix(
+        'bal_bal_src1_').unstack()
+    train_X_tr_uid2_bal_src1_bal.columns = [x[0] + "_" + str(x[1]) for x in
+                                            train_X_tr_uid2_bal_src1_bal.columns.ravel()]
+    train_X_tr_uid2_bal_src1_bal.reset_index(inplace=True)
+    train_X_tr_uid2_bal_src1_bal.fillna(0.0, inplace=True)
+    print(train_X_tr_uid2_bal_src1_bal.head(5))
+
+    ####################
+    train_X_tr_uid2_channel_bal = train_X_tr.groupby(['UID', 'channel'])['bal'].agg(
+        {'mean', 'max', 'min'}).add_prefix(
+        'bal_channel_').unstack()
+    train_X_tr_uid2_channel_bal.columns = [x[0] + "_" + str(x[1]) for x in train_X_tr_uid2_channel_bal.columns.ravel()]
+    train_X_tr_uid2_channel_bal.reset_index(inplace=True)
+    train_X_tr_uid2_channel_bal.fillna(0.0, inplace=True)
+    print(train_X_tr_uid2_channel_bal.head(5))
+
+    train_X_tr_uid2_market_type_bal = train_X_tr.groupby(['UID', 'market_type'])['bal'].agg(
+        {'mean', 'max', 'min'}).add_prefix(
+        'bal_market_type_').unstack()
+    train_X_tr_uid2_market_type_bal.columns = [x[0] + "_" + str(x[1]) for x in
+                                               train_X_tr_uid2_market_type_bal.columns.ravel()]
+    train_X_tr_uid2_market_type_bal.reset_index(inplace=True)
+    train_X_tr_uid2_market_type_bal.fillna(0.0, inplace=True)
+    print(train_X_tr_uid2_market_type_bal.head(5))
+
+    train_X_tr_uid2_trans_type2_bal = train_X_tr.groupby(['UID', 'trans_type2'])['bal'].agg(
+        {'mean', 'max', 'min'}).add_prefix(
+        'bal_trans_type2_').unstack()
+    train_X_tr_uid2_trans_type2_bal.columns = [x[0] + "_" + str(x[1]) for x in
+                                               train_X_tr_uid2_trans_type2_bal.columns.ravel()]
+    train_X_tr_uid2_trans_type2_bal.reset_index(inplace=True)
+    train_X_tr_uid2_trans_type2_bal.fillna(0.0, inplace=True)
+    print(train_X_tr_uid2_trans_type2_bal.head(5))
+
+    train_X_tr_uid2_trans_type1_bal = train_X_tr.groupby(['UID', 'trans_type1'])['bal'].agg(
+        {'mean', 'max', 'min'}).add_prefix(
+        'bal_trans_type1_').unstack()
+    train_X_tr_uid2_trans_type1_bal.columns = [x[0] + "_" + str(x[1]) for x in
+                                               train_X_tr_uid2_trans_type1_bal.columns.ravel()]
+    train_X_tr_uid2_trans_type1_bal.reset_index(inplace=True)
+    train_X_tr_uid2_trans_type1_bal.fillna(0.0, inplace=True)
+    print(train_X_tr_uid2_trans_type1_bal.head(5))
+
+    train_X_tr_uid2_bal_src1_bal = train_X_tr.groupby(['UID', 'amt_src1'])['bal'].agg(
+        {'mean', 'max', 'min'}).add_prefix(
+        'amt_bal_src1_').unstack()
+    train_X_tr_uid2_bal_src1_bal.columns = [x[0] + "_" + str(x[1]) for x in
+                                            train_X_tr_uid2_bal_src1_bal.columns.ravel()]
+    train_X_tr_uid2_bal_src1_bal.reset_index(inplace=True)
+    train_X_tr_uid2_bal_src1_bal.fillna(0.0, inplace=True)
+    print(train_X_tr_uid2_bal_src1_bal.head(5))
+
+
 
 
     train_X_tr.head(5)
